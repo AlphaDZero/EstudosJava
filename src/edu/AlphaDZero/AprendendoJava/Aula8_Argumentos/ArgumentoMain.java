93465b853ab75854a1694fce1868b06450998960
AlphaDZero <andrebezerque@gmail.com>
2023-03-07 20:35:54 -0300

    Aula8

---
+++ src/edu/AlphaDZero/AprendendoJava/Aula8_Argumentos/ArgumentoMain.java
@@ -0,0 +1,17 @@
+package edu.AlphaDZero.AprendendoJava.Aula8_Argumentos;
+
+
+public class ArgumentoMain {
+    public static void main(String[] args) {
+        String nome      = args[0];
+        String sobrenome = args[1];
+        int idade        = Integer.valueOf(args[2]);
+        double altura    = Double.valueOf(args[3]);
+
+        System.out.println("Olá, sou "+ nome+ " "+sobrenome);
+        System.out.println("tenho "+idade+" anos");
+        System.out.println("minha autura é "+altura);
+
+    }
+    
+}

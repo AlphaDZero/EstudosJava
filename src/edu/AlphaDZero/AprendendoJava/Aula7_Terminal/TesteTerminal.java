c92c1b227da116c9523e31cd46dcc1aaecefcf56
AlphaDZero <andrebezerque@gmail.com>
2023-03-07 20:05:13 -0300

    Aula7

---
+++ src/edu/AlphaDZero/AprendendoJava/Aula7_Terminal/TesteTerminal.java
@@ -0,0 +1,7 @@
+package edu.AlphaDZero.AprendendoJava.Aula7_Terminal;
+
+public class TesteTerminal {
+    public static void main(String[] args) {
+        System.out.println("Essa mensagem foi mostrada no terminal");
+    }
+}

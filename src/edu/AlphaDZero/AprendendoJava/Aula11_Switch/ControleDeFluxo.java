fbf0c4643bfd5bbb5304e723da2bc21c39172d32
AlphaDZero <andrebezerque@gmail.com>
2023-03-10 17:19:41 -0300

    Aula11

---
+++ src/edu/AlphaDZero/AprendendoJava/Aula11_Switch/ControleDeFluxo.java
@@ -0,0 +1,23 @@
+package edu.AlphaDZero.AprendendoJava.Aula11_Switch;
+
+import java.util.Scanner;
+
+public class ControleDeFluxo {
+    public static void main(String[] args) {
+        System.out.print("Digite a senha: ");
+        Scanner scanner = new Scanner(System.in);
+        String senha = scanner.next();scanner.close();
+
+        switch(senha){
+            case "20211040100046":
+                System.out.println("Seja bem vindo, AlphaZero");
+                break;
+            case "20221045108711":
+                System.out.println("Seja bem vindo, Elden Lord");
+                break;
+            default:
+                System.out.println("Senha incorreta");
+                break;
+        }
+    }
+}

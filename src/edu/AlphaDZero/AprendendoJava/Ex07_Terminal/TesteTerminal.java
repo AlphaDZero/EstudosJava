8e3e8a2a5e54cd1ea9d830e523f5993dcf9ddcb5
AlphaDZero <andrebezerque@gmail.com>
2023-03-14 12:04:57 -0300

    Atualização

--- src/edu/AlphaDZero/AprendendoJava/Ex07_Terminal/TesteTerminal.java
+++ src/edu/AlphaDZero/AprendendoJava/Ex07_Terminal/TesteTerminal.java
@@ -2,6 +2,19 @@ package edu.AlphaDZero.AprendendoJava.Ex07_Terminal;
 
 public class TesteTerminal {
     public static void main(String[] args) {
-        System.out.println("Essa mensagem foi mostrada no terminal");
+        String mensagem ="";
+        for (String string : args) {
+            mensagem += string;
+        }
+        try{
+            final String caminho, comando;
+            caminho = "C:/Users/Marques/OneDrive/Documentos/AlphaProject/AlphaDJava/src/edu/AlphaDZero/AprendendoJava/Ex07_Terminal";
+            comando = "echo "+mensagem+" > arq.txt";
+            Runtime.getRuntime().exec("cmd.exe /c cd "+caminho);
+            Runtime.getRuntime().exec("cmd.exe /c"+comando);
+        }
+        catch(Exception e){
+            System.out.println("Error "+e);
+        }
     }
 }
---
+++ src/edu/AlphaDZero/AprendendoJava/Ex15_Arrays/TesteVetor2.java
@@ -0,0 +1,10 @@
+package edu.AlphaDZero.AprendendoJava.Ex15_Arrays;
+
+public class TesteVetor2 {
+    public static void main(String[] args) {
+        int[] vetor = {1,2,3,4,5,6};
+        for (int i : vetor) {
+            System.out.print(i+" ");
+        }
+    }
+}

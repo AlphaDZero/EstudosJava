e0cbeb43af6db3360bd906830deb2e3f7562d153
AlphaDZero <andrebezerque@gmail.com>
2023-03-30 17:41:05 -0300

    atualização

--- src/edu/AlphaDZero/AprendendoJava/EstruturaDeDados/Ex05_Filas/Fila.java
+++ src/edu/AlphaDZero/AprendendoJava/EstruturaDeDados/Ex05_Filas/Fila.java
@@ -1,5 +1,26 @@
 package edu.AlphaDZero.AprendendoJava.EstruturaDeDados.Ex05_Filas;
 
 public class Fila {
+    private No ultimof = null;
+
+    public Fila() {}
+
+    public boolean isEmpty(){
+        return this.ultimof == null ? true :false;
+    }
+
+    public void enqueue(No novo){
+        novo.setUltimo(ultimof);
+        ultimof = novo;
+    }
+
+    public No first(){
+        if(!this.isEmpty()){
+            
+        }
+        return null;
+    }
+
     
+
 }

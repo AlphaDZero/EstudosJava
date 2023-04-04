8841a0b85b83299d81682b2b51b579d2ca63ee88
AlphaDZero <andrebezerque@gmail.com>
2023-04-04 18:31:21 -0300

    Métodos

---
+++ src/edu/AlphaDZero/AprendendoJava/Métodos/Ex01/Ex01.java
@@ -0,0 +1,13 @@
+package edu.AlphaDZero.AprendendoJava.Métodos.Ex01;
+/**
+ * <h2>Questão 1</h2><br/>
+ * <h3>Criar uma aplicação que resolva as seguintes situações:</h3><br/>
+ * <ul>
+ *      <li><p>Calcule as 4 operações básicas: soma, multiplicação, subtração e divisão.</p></li>
+ *      <li><p>A partir da hora do dia, informe a mensagem adequada: "Bom dia", "Boa tarde" e "Boa noite".</p></li>
+ *      <li><p>Calcule o valor final de um empréstimo, a partir do valor solicitado. Taxas e parcelas influenciam. Defina arbitrariamente as faixas que influenciam no valor</p></li>
+ * </ul>
+ */
+public class Ex01 {
+    
+}
